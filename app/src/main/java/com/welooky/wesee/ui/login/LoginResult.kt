cfac5c3c2c133254de36aa98744949834f4588c2
zhangsheng <zhsheng26@gmail.com>
2021-05-19 23:26:37 +0800

    init

---
+++ app/src/main/java/com/welooky/wesee/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.welooky.wesee.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
